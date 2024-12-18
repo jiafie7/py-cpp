a60d7d7cd66e654cc082c2725ce466a03e68dd55
jiafie <67643874+jiafie7@users.noreply.github.com>
2024-12-18 14:30:39 +0800

    commit python function call with parameters in cpp

--- main.cpp
+++ main.cpp
@@ -13,20 +13,36 @@ int main()
   PyRun_SimpleString("import sys");
   PyRun_SimpleString("sys.path.append('./../script')");
 
-  PyObject* module = PyImport_ImportModule("add");
-  if (module == nullptr)
+  PyObject* test1 = PyImport_ImportModule("test1");
+  if (test1 == nullptr)
   {
     std::cout << "module import failed." << '\n';
     return -1;
   }
-  
-  PyObject* func = PyObject_GetAttrString(module, "add");
-  if (func == nullptr)
+
+  // ------------------------
+  PyObject* say = PyObject_GetAttrString(test1, "say");
+  if (say == nullptr)
   {
     std::cout << "funtion not found." << '\n';
     return -1;
   }
-  if (!PyCallable_Check(func))
+  if (!PyCallable_Check(say))
+  {
+    std::cout << "not callable obejct." << '\n';
+    return -1;
+  }
+
+  PyObject_CallObject(say, nullptr);
+    
+  // -------------------------
+  PyObject* add = PyObject_GetAttrString(test1, "add");
+  if (add == nullptr)
+  {
+    std::cout << "funtion not found." << '\n';
+    return -1;
+  }
+  if (!PyCallable_Check(add))
   {
     std::cout << "not a callable object." << '\n';
     return -1;
@@ -36,12 +52,14 @@ int main()
   PyTuple_SetItem(args, 0, Py_BuildValue("i", 1));
   PyTuple_SetItem(args, 1, Py_BuildValue("i", 2));
 
-  PyObject* res = PyObject_CallObject(func, args);
+  PyObject* res = PyObject_CallObject(add, args);
   
   int result;
   PyArg_Parse(res, "i", &result);
 
   std::cout << "result = " << result << '\n';
+
+  // ---------------------------
 
   Py_Finalize();
 
---
+++ script/test1.py
@@ -0,0 +1,8 @@
+def say():
+    print("hello python!")
+
+
+def add(a, b):
+    c = a + b
+    print(f"{a} + {b} = {c}")
+    return c
